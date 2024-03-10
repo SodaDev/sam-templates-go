7261795b0836d1f71371f25ebf6345741478410b
sodkiewiczm <sodkiewiczm@gmail.com>
2024-03-10 21:28:22 +0100

    Fix integration test import

--- {{cookiecutter.module_name}}/cmd/handler/handler_test.go
+++ {{cookiecutter.module_name}}/cmd/handler/handler_test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"github.com/Ryanair/gofrlib-test/frContainers"
 	"github.com/Ryanair/gofrlib-test/testDefaults"
-	"github.com/testcontainers/testcontainers-go"
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
